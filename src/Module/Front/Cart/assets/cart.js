d06816b915b4662297dc78dea9d5f254f7ceeba3
Simon Asika <asika32764@gmail.com>
2023-02-11 22:30:15 +0800

    cart wip

--- src/Module/Front/Cart/assets/cart.js
+++ src/Module/Front/Cart/assets/cart.js
@@ -1,1 +1,43 @@
 // JS file for Cart
+
+import '@main';
+
+await u.domready();
+
+const { createApp, ref, toRefs, reactive, computed, watch, provide, nextTick, onMounted } = Vue;
+
+const CartApp = {
+  name: 'CartApp',
+  props: {
+
+  },
+  setup(props) {
+    const state = reactive({
+      items: [],
+      totals: [],
+      coupons: [],
+      loading: false
+    });
+
+    init();
+
+    function init() {
+
+    }
+
+    async function loadItems() {
+      const res = await u.$http.get('@cart_ajax/getItems');
+
+
+    }
+
+    return {
+
+    };
+  }
+};
+
+const app = createApp(CartApp, u.data('cart.props'));
+
+app.use(ShopGoVuePlugin);
+app.mount('cart-app');
--- src/Module/Front/Cart/views/cart.blade.php
+++ src/Module/Front/Cart/views/cart.blade.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App\View;
+
 /**
  * Global variables
  * --------------------------------------------------------------
@@ -12,8 +16,10 @@
  * @var $lang      LangService     The language translation service.
  */
 
-declare(strict_types=1);
-
+use Lyrasoft\ShopGo\Script\ShopGoScript;
+use Unicorn\Image\ImagePlaceholder;
+use Unicorn\Script\UnicornScript;
+use Unicorn\Script\VueScript;
 use Windwalker\Core\Application\AppContext;
 use Windwalker\Core\Asset\AssetService;
 use Windwalker\Core\DateTime\ChronosService;
@@ -21,10 +27,52 @@ use Windwalker\Core\Language\LangService;
 use Windwalker\Core\Router\Navigator;
 use Windwalker\Core\Router\SystemUri;
 
+$imagePlaceholder = $app->service(ImagePlaceholder::class);
+
+$shopGoScript = $app->service(ShopGoScript::class);
+$shopGoScript->vueUtilities();
+$shopGoScript->productCart();
+
+$vueScript = $app->service(VueScript::class);
+$vueScript->vue();
+$vueScript->animate();
+
+$uniScript = $app->service(UnicornScript::class);
+$uniScript->data('product.item.props', [
+
+]);
+$uniScript->data('image.default', $imagePlaceholder->placeholderSquare());
+
+$uniScript->addRoute('@cart_ajax');
 ?>
 
 @extends('global.body')
 
 @section('content')
-    <h2>Cart view</h2>
+    <div class="l-cart-page container my-5">
+        <cart-app>
+            <div class="row">
+                <div class="col-lg-9 l-cart-page__content">
+                    <header class="d-flex align-items-center justify-content-between mb-4">
+                        <h3 class="m-0">購物車</h3>
+
+                        <div>
+                            <a href="javascript://">
+                                <i class="fa fa-times"></i>
+                                移除所有商品
+                            </a>
+                        </div>
+                    </header>
+
+                    <div class="l-cart-items">
+
+                    </div>
+                </div>
+
+                <div class="col-lg-3 l-cart-page__sidebar">
+
+                </div>
+            </div>
+        </cart-app>
+    </div>
 @stop
