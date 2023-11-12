754246db1cab6d878b09dcf699d5878c3b786505
Simon Asika <asika32764@gmail.com>
2023-11-12 17:01:06 +0800

    no menu

--- resources/menu/front/mainmenu.menu.php
+++ resources/menu/front/mainmenu.menu.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Part of earth project.
- *
- * @copyright  Copyright (C) 2021 __ORGANIZATION__.
- * @license    __LICENSE__
- */
-
 declare(strict_types=1);
 
 namespace App\Menu;
@@ -48,58 +41,6 @@ $menu->registerChildren(
                 $menu->link(
                     $node->getValue()->title,
                     $nav->to('article_category')->var('path', $node->getValue()->path)
-                );
-            }
-        );
-    }
-);
-
-// Portfolio
-$menu->link('案例', $nav->to('portfolio_category'))
-    ->icon('fal fa-files');
-
-$menu->registerChildren(
-    function (MenuBuilder $menu) use ($app, $nav) {
-        $categories = $app->service(CategoryRepository::class)
-            ->getListSelector()
-            ->where('category.state', 1)
-            ->where('category.type', 'portfolio')
-            ->ordering('category.lft', 'ASC');
-
-        $categoryNodes = TreeBuilder::create($categories);
-
-        $menu->fromTree(
-            $categoryNodes,
-            function (NodeInterface $node, MenuBuilder $menu) use ($nav) {
-                $menu->link(
-                    $node->getValue()->title,
-                    $nav->to('portfolio_category')->var('path', $node->getValue()->path)
-                );
-            }
-        );
-    }
-);
-
-// Members
-$menu->link('團隊成員', $nav->to('member_category'))
-    ->icon('fal fa-users');
-
-$menu->registerChildren(
-    function (MenuBuilder $menu) use ($app, $nav) {
-        $categories = $app->service(CategoryRepository::class)
-            ->getListSelector()
-            ->where('category.state', 1)
-            ->where('category.type', 'member')
-            ->ordering('category.lft', 'ASC');
-
-        $categoryNodes = TreeBuilder::create($categories);
-
-        $menu->fromTree(
-            $categoryNodes,
-            function (NodeInterface $node, MenuBuilder $menu) use ($nav) {
-                $menu->link(
-                    $node->getValue()->title,
-                    $nav->to('member_category')->var('path', $node->getValue()->path)
                 );
             }
         );
