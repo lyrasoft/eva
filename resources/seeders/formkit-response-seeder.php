3a33d37b8f7f85b1b74fb6422729a88cc1943e3a
Simon Asika <asika32764@gmail.com>
2024-08-19 22:03:20 +0800

    response admin wip

---
+++ resources/seeders/formkit-response-seeder.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Seeder;
+
+use Windwalker\Core\Seed\Seeder;
+use Windwalker\Database\DatabaseAdapter;
+use Windwalker\ORM\ORM;
+
+/**
+ * FormkitResponse Seeder
+ *
+ * @var Seeder          $seeder
+ * @var ORM             $orm
+ * @var DatabaseAdapter $db
+ */
+$seeder->import(
+    static function () use ($seeder, $orm, $db) {
+        $faker = $seeder->faker('en_US');
+
+        //
+    }
+);
+
+$seeder->clear(
+    static function () use ($seeder, $orm, $db) {
+        //
+    }
+);
