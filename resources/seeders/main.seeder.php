fced7c604a4bf6a3d19b3902a7fc999fb5b4f92c
Simon Asika <asika32764@gmail.com>
2026-01-20 21:49:08 +0800

    contact seeders

--- resources/seeders/main.seeder.php
+++ resources/seeders/main.seeder.php
@@ -11,6 +11,7 @@ return [
     __DIR__ . '/category.seeder.php',
     __DIR__ . '/tag.seeder.php',
     __DIR__ . '/article.seeder.php',
+    __DIR__ . '/contact.seeder.php',
     __DIR__ . '/page.seeder.php',
     __DIR__ . '/menu.seeder.php',
     __DIR__ . '/widget.seeder.php',
